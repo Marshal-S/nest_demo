6c16bdb312271104f8b0e1820b22289748da0d60
Marshal <>
2023-09-11 15:23:53 +0800

    打开自动同步，可以避免一些问题

--- src/file/entities/file.entity.ts
+++ src/file/entities/file.entity.ts
@@ -1,13 +1,12 @@
-import { Blob } from "buffer";
-import { User } from "src/user/entities/user.entity";
-import { Binary, Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class File {
     @PrimaryGeneratedColumn()
     id: number
 
-    @Column({ default: 0 })
+    //设置一个大类型,8个字节，默认int只有4个字节，small 2 字节，mediumint 3字节
+    @Column('bigint', { default: 0})
     size: number;
 
     @Column({ default: null })
@@ -19,6 +18,10 @@ export class File {
     //图片路径，客户端使用时，需要拼接 host
     @Column({ default: null })
     path: string
+
+    //设置一个bool类型实际就是tinyint，1字节
+    @Column('bool', { default: 0})
+    isDelete: number
 
     //我们跟用户建立关系，直到是谁创建的，图片出问题方便追寻，这里不用即可，需要时引用到关系表最好
     // @ManyToOne(() => User, user => user)
